5503f0326099cb4d2d6910f35a86b438e02e8448
Phil Ratzloff <pratzl@outlook.com>
2021-12-24 10:27:25 -0500

    Add cmake directory (not used yet)

---
+++ cmake/Conan.cmake
@@ -0,0 +1,38 @@
+macro(run_conan)
+  # Download automatically, you can also just copy the conan.cmake file
+  if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/f8fd649b3058846188d8b12040c354fd50e30780/conan.cmake"
+                  "${CMAKE_BINARY_DIR}/conan.cmake"
+                  EXPECTED_HASH SHA256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+                  TLS_VERIFY ON)
+    # The following should be used, but doesn't work': 'DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.17.0/conan.cmake"
+  endif()
+
+  set (ENV{CONAN_REVISIONS_ENABLED} 1)
+  include(${CMAKE_BINARY_DIR}/conan.cmake)
+
+# The following is set in ~/.conan/profiles/default
+#    compiler=gcc
+#    compiler.version=11
+#
+
+# note: conan_cmake_run is deprecated and should be updated with new code shown
+#       at https://github.com/conan-io/cmake-conan
+
+conan_cmake_run(
+  REQUIRES
+  ${CONAN_EXTRA_REQUIRES}
+  catch2/2.13.7
+  docopt.cpp/0.6.2
+  fmt/8.0.1
+  spdlog/1.9.2
+  range-v3/0.11.0
+  OPTIONS
+  ${CONAN_EXTRA_OPTIONS}
+  SETTINGS compiler.cppstd=20 
+  BASIC_SETUP
+  CMAKE_TARGETS # individual targets to link to
+  BUILD
+  missing)
+endmacro()
