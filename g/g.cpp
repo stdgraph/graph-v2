0e0fd26b46202f259f358c90db9c71740394d03f
Phil Ratzloff <pratzl@outlook.com>
2021-12-30 12:47:25 -0500

    Add output to verify Linux/Windows x Debug/Release

--- g/g.cpp
+++ g/g.cpp
@@ -5,8 +5,19 @@
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+int main() {
+  cout << "Hello CMake";
+#ifdef WIN32
+  cout << " on Windows";
+#else
+  cout << " on Linux";
+#endif
+#if defined(_DEBUG) || !defined(NDEBUG)
+  cout << " (Debug)";
+#else
+  cout << " (Release)";
+#endif
+  cout << "." << endl;
+
+  return 0;
 }
