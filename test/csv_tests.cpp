92ab9b1685fb7d058cafa6f983a55ef215bc79d6
Phil Ratzloff <pratzl@outlook.com>
2022-01-08 17:06:09 -0500

    Add TEST_DATA_ROOT_DIR definition and pass to test programs
    csv_tests can now read germany_routes.csv

--- test/csv_tests.cpp
+++ test/csv_tests.cpp
@@ -1,4 +1,17 @@
 #include <catch2/catch.hpp>
 #include "csv_parser.hpp"
 
-TEST_CASE("Dummy CSV test", "[csv]") { REQUIRE(1 == 1); }
+using namespace csv;
+
+TEST_CASE("Dummy CSV test", "[csv]") {
+  CSVReader reader(TEST_DATA_ROOT_DIR "germany_routes.csv");
+
+  for (CSVRow& row : reader) { // Input iterator
+    for (CSVField& field : row) {
+      // By default, get<>() produces a std::string.
+      // A more efficient get<string_view>() is also available, where the resulting
+      // string_view is valid as long as the parent CSVRow is alive
+      //std::cout << field.get<>() << ...
+    }
+  }
+}
