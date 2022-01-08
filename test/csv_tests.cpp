61cb966f773953b36864fc6b1849d119d0a6ca64
Phil Ratzloff <pratzl@outlook.com>
2022-01-08 16:26:48 -0500

    Disable warnings for csv.h and move to new csv_parser.hpp

--- test/csv_tests.cpp
+++ test/csv_tests.cpp
@@ -1,10 +1,4 @@
-
-
-#include "single_include/csv.hpp"
 #include <catch2/catch.hpp>
+#include "csv_parser.hpp"
 
-//static_assert(__cplusplus >= 201703L, "C++ version?");
-
-TEST_CASE("Dummy CSV test", "[csv]") {
-  REQUIRE(1 == 1);
-}
+TEST_CASE("Dummy CSV test", "[csv]") { REQUIRE(1 == 1); }
