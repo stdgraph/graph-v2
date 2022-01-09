74b6e5826983d4f52b0152c9807b6b4aa19cb871
Phil Ratzloff <pratzl@outlook.com>
2022-01-08 21:03:55 -0500

    Add bktest1.mtx & bktest2.mtx from NWGr for future testing
    Remove obsolete code in csv_tests

--- test/csv_tests.cpp
+++ test/csv_tests.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch.hpp>
 #include "csv_parser.hpp"
 #ifdef _MSC_VER
-#include "Windows.h"
+#  include "Windows.h"
 #endif
 
 using namespace csv;
@@ -13,30 +13,13 @@ TEST_CASE("Dummy CSV test", "[csv]") {
 #ifdef _MSC_VER
   SetConsoleOutputCP(CP_UTF8);
 #endif
-  setvbuf(stdout, nullptr, _IOFBF, 1000);
+  setvbuf(stdout, nullptr, _IOFBF, 1000); // avoid shearing multi-byte characters across buffer boundaries
   CSVReader reader(TEST_DATA_ROOT_DIR "germany_routes.csv");
 
   for (CSVRow& row : reader) { // Input iterator
-
-#if 1
     string_view from = row[0].get<string_view>();
     string_view to   = row[1].get<string_view>();
-    int         dist = row[2].get<int>();
+    auto        dist = row[2].get<double>();
     cout << from << "," << to << "," << dist << endl;
-#else
-    bool first = true;
-    for (CSVField& field : row) {
-      if (first) {
-        first = false;
-      } else {
-        cout << ",";
-      }
-      // By default, get<>() produces a std::string.
-      // A more efficient get<string_view>() is also available, where the resulting
-      // string_view is valid as long as the parent CSVRow is alive
-      cout << field.get<>();
-    }
-    cout << endl;
-#endif
   }
 }
