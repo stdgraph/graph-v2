dbe7050494c7e94b882eb14e84a64c1ff246bfc0
Phil Ratzloff <pratzl@outlook.com>
2022-01-27 19:12:49 -0500

    Rename dijkstra algo in tests to dijkstra_book & move to algortihm/dijkstra_book.hpp

--- test/csv_routes_tests.cpp
+++ test/csv_routes_tests.cpp
@@ -2,16 +2,12 @@
 #include "csv_routes_csr.hpp"
 #include "csv_routes_vol.hpp"
 #include "graph/graph.hpp"
-//#include "graph/adaptor/vertex_range_adaptor.hpp"
+#include "graph/algorithm/dijkstra_book.hpp"
 #include "graph/view/vertices_view.hpp"
 #include <cassert>
 #ifdef _MSC_VER
 #  include "Windows.h"
 #endif
-
-// for djikstra
-#include <queue>
-#include <tuple>
 
 #define TEST_OPTION_OUTPUT (1)
 #define TEST_OPTION_GEN (2)
@@ -114,64 +110,12 @@ OStream& operator<<(OStream& os, const ostream_indenter& osi) {
 }
 
 
-template <typename G, typename F>
-concept edge_weight_function = // e.g. weight(uv)
-      std::copy_constructible<F> && std::regular_invocable<F&, std::ranges::range_reference_t<vertex_edge_range_t<G>>>;
-
-// The index into weight vector stored as the first property
-template <std::graph::incidence_graph G, typename WF>
-requires edge_weight_function<G, WF> &&
-      std::is_arithmetic_v<std::invoke_result_t<WF, std::ranges::range_reference_t<vertex_edge_range_t<G>>>> &&
-      std::ranges::random_access_range<std::graph::vertex_range_t<G>>
-auto dijkstra(
-      G&& g, vertex_key_t<G> source, WF weight = [](std::ranges::range_reference_t<vertex_edge_range_t<G>> uv) {
-        return 1;
-      }) {
-  using vertex_key_type = vertex_key_t<G>;
-  using weight_type     = decltype(weight(std::declval<std::ranges::range_reference_t<vertex_edge_range_t<G>>>()));
-
-  size_t N(size(g));
-  assert(source < N);
-
-  std::vector<weight_type> distance(N, std::numeric_limits<weight_type>::max());
-  distance[source] = 0;
-
-  using weighted_vertex = std::tuple<vertex_key_type, weight_type>;
-
-  std::priority_queue<weighted_vertex, std::vector<weighted_vertex>,
-                      decltype([](auto&& a, auto&& b) { return (std::get<1>(a) > std::get<1>(b)); })>
-        Q;
-
-  Q.push({source, distance[source]});
-
-  while (!Q.empty()) {
-
-    auto u = std::get<0>(Q.top());
-    Q.pop();
-
-    // for (auto&& [v, w] : g[u]) -- pretty but would only allow one property
-
-    //auto vw = incidence_edges_view(g, u, target_key); // (can't put it in for stmt below right now)
-    //for (auto&& [v, uv] : vw) {
-    for (auto&& uv : std::graph::edges(g, g[u])) {
-      auto        v = target_key(g, uv);
-      weight_type w = weight(uv);
-      if (distance[u] + w < distance[v]) {
-        distance[v] = distance[u] + w;
-        Q.push({v, distance[v]});
-      }
-    }
-  }
-
-  return distance;
-}
-
 TEST_CASE("Germany routes CSV+csr test", "[csv][csr]") {
   init_console();
   routes_csv_csr_graph germany_routes(TEST_DATA_ROOT_DIR "germany_routes.csv");
 }
 
-TEST_CASE("Germany routes CSV+vol dijkstra", "[csv][vol][germany][dijkstra]") {
+TEST_CASE("Germany routes CSV+vol dijkstra_book", "[csv][vol][germany][dijkstra][book]") {
   init_console();
   routes_vol_graph germany_routes(TEST_DATA_ROOT_DIR "germany_routes.csv");
   using G = routes_vol_graph::graph_type;
@@ -179,7 +123,7 @@ TEST_CASE("Germany routes CSV+vol dijkstra", "[csv][vol][germany][dijkstra]") {
 
   auto frankfurt_key = germany_routes.frankfurt_key();
   auto weight        = [&g](std::ranges::range_reference_t<vertex_edge_range_t<G>> uv) { return edge_value(g, uv); };
-  auto result        = dijkstra(g, frankfurt_key, weight);
+  auto result        = std::graph::dijkstra_book(g, frankfurt_key, weight);
 }
 
 
