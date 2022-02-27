7d0dfb8714ee20975c0f32603ca04b094c9ae90c
Phil Ratzloff <pratzl@outlook.com>
2022-02-27 17:14:23 -0500

    rename edgelist iterators for clarity

--- include/graph/views/edgelist_view.hpp
+++ include/graph/views/edgelist_view.hpp
@@ -10,16 +10,16 @@
 namespace std::graph::views {
 
 template <class G>
-class const_edge_view_iterator;
+class const_edgelist_iterator;
 template <class G>
-class edge_view_iterator;
+class edgelist_iterator;
 
 
 template <class G>
 requires ranges::forward_range<vertex_range_t<G>>
 constexpr auto edgelist_view(const G& g) {
   using vertex_type   = remove_cvref_t<decltype(u)>;
-  using iter_type     = const_edge_view_iterator<const G>;
+  using iter_type     = const_edgelist_iterator<const G>;
   using sentinal_type = typename iter_type::edge_iterator;
   using SR            = ranges::subrange<iter_type, sentinal_type>;
 
@@ -32,7 +32,7 @@ constexpr auto edgelist_view(const G& g) {
 template <class G>
 requires ranges::forward_range<vertex_range_t<G>>
 constexpr auto edgelist_view(G& g) {
-  using iter_type     = edge_view_iterator<G>;
+  using iter_type     = edgelist_iterator<G>;
   using sentinal_type = typename iter_type::edge_iterator;
   using SR            = ranges::subrange<iter_type, sentinal_type>;
 
@@ -43,7 +43,7 @@ constexpr auto edgelist_view(G& g) {
 
 
 template <class G>
-class const_edge_view_iterator {
+class const_edgelist_iterator {
 public:
   using graph_type = remove_cvref_t<G>;
 
@@ -69,21 +69,21 @@ protected:
   using shadow_value_type = pair<vertex_key_type, vertex_key_type, edge_type*>;
 
 public:
-  const_edge_view_iterator(const graph_type& g, vertex_iterator vtx_iter, edge_iterator edg_iter)
+  const_edgelist_iterator(const graph_type& g, vertex_iterator vtx_iter, edge_iterator edg_iter)
         : g_(&const_cast<graph_type&>(g)), vtx_iter_(vtx_iter), edg_iter_(edg_iter) {
     find_first_non_empty();
   }
-  const_edge_view_iterator(const graph_type& g, vertex_iterator vtx_iter)
-        : const_edge_view_iterator(
+  const_edgelist_iterator(const graph_type& g, vertex_iterator vtx_iter)
+        : const_edgelist_iterator(
                 g, vtx_iter, ranges::begin(edges(const_cast<graph_type&>(g), const_cast<vertex_type&>(*vtx_iter)))) {}
 
-  constexpr const_edge_view_iterator()                                = default;
-  constexpr const_edge_view_iterator(const const_edge_view_iterator&) = default;
-  constexpr const_edge_view_iterator(const_edge_view_iterator&&)      = default;
-  constexpr ~const_edge_view_iterator()                               = default;
+  constexpr const_edgelist_iterator()                                = default;
+  constexpr const_edgelist_iterator(const const_edgelist_iterator&) = default;
+  constexpr const_edgelist_iterator(const_edgelist_iterator&&)      = default;
+  constexpr ~const_edgelist_iterator()                               = default;
 
-  constexpr const_edge_view_iterator& operator=(const const_edge_view_iterator&) = default;
-  constexpr const_edge_view_iterator& operator=(const_edge_view_iterator&&) = default;
+  constexpr const_edgelist_iterator& operator=(const const_edgelist_iterator&) = default;
+  constexpr const_edgelist_iterator& operator=(const_edgelist_iterator&&) = default;
 
 public:
   constexpr reference operator*() const {
@@ -91,18 +91,18 @@ public:
     return reinterpret_cast<reference>(value_);
   }
 
-  constexpr const_edge_view_iterator& operator++() {
+  constexpr const_edgelist_iterator& operator++() {
     next();
     return *this;
   }
-  constexpr const_edge_view_iterator operator++(int) const {
-    const_edge_view_iterator tmp(*this);
+  constexpr const_edgelist_iterator operator++(int) const {
+    const_edgelist_iterator tmp(*this);
     ++*this;
     return tmp;
   }
 
-  constexpr bool operator==(const const_edge_view_iterator& rhs) const { return vtx_iter_ == rhs.vtx_iter_ && edg_iter_ == rhs.edg_iter_; }
-  //constexpr bool operator==(const edge_view_iterator& rhs) const { return edg_iter_ == rhs; }
+  constexpr bool operator==(const const_edgelist_iterator& rhs) const { return vtx_iter_ == rhs.vtx_iter_ && edg_iter_ == rhs.edg_iter_; }
+  //constexpr bool operator==(const edgelist_iterator& rhs) const { return edg_iter_ == rhs; }
 protected:
   void find_first_non_empty() {
     // skip past any vertices with no incidence edges
@@ -128,13 +128,13 @@ protected:
   vertex_iterator           vtx_iter_;
   edge_iterator             edg_iter_;
 
-  //friend bool operator==(const edge_iterator& lhs, const const_edge_view_iterator& rhs) { return lhs == rhs.edg_iter_; }
+  //friend bool operator==(const edge_iterator& lhs, const const_edgelist_iterator& rhs) { return lhs == rhs.edg_iter_; }
 };
 
 template <class G>
-class edge_view_iterator : public const_edge_view_iterator<G> {
+class edgelist_iterator : public const_edgelist_iterator<G> {
 public:
-  using base_type = const_edge_view_iterator<G>;
+  using base_type = const_edgelist_iterator<G>;
 
   using graph_type = G;
 
@@ -164,16 +164,16 @@ protected:
   using base_type::next;
 
 public:
-  edge_view_iterator(graph_type& g, vertex_iterator vtx_iter, edge_iterator edg_iter) : base_type(g, vtx_iter, edg_iter) {}
-  edge_view_iterator(graph_type& g, itertex_iterator vtx_itr) : base_type(g, vtx_iter) {}
+  edgelist_iterator(graph_type& g, vertex_iterator vtx_iter, edge_iterator edg_iter) : base_type(g, vtx_iter, edg_iter) {}
+  edgelist_iterator(graph_type& g, itertex_iterator vtx_itr) : base_type(g, vtx_iter) {}
 
-  constexpr edge_view_iterator()                          = default;
-  constexpr edge_view_iterator(const edge_view_iterator&) = default;
-  constexpr edge_view_iterator(edge_view_iterator&&)      = default;
-  constexpr ~edge_view_iterator()                         = default;
+  constexpr edgelist_iterator()                          = default;
+  constexpr edgelist_iterator(const edgelist_iterator&) = default;
+  constexpr edgelist_iterator(edgelist_iterator&&)      = default;
+  constexpr ~edgelist_iterator()                         = default;
 
-  constexpr edge_view_iterator& operator=(const edge_view_iterator&) = default;
-  constexpr edge_view_iterator& operator=(edge_view_iterator&&) = default;
+  constexpr edgelist_iterator& operator=(const edgelist_iterator&) = default;
+  constexpr edgelist_iterator& operator=(edgelist_iterator&&) = default;
 
 public:
   constexpr reference operator*() const {
@@ -181,23 +181,23 @@ public:
     return reinterpret_cast<reference>(value_);
   }
 
-  constexpr edge_view_iterator& operator++() {
+  constexpr edgelist_iterator& operator++() {
     next();
     return *this;
   }
-  constexpr edge_view_iterator operator++(int) const {
-    edge_view_iterator tmp(*this);
+  constexpr edgelist_iterator operator++(int) const {
+    edgelist_iterator tmp(*this);
     ++*this;
     return tmp;
   }
 
-  constexpr bool operator==(const edge_view_iterator& rhs) const {
+  constexpr bool operator==(const edgelist_iterator& rhs) const {
     return vtx_iter_ == rhs.vtx_iter_ && edg_iter_ == rhs.edg_iter_;
   }
-  //constexpr bool operator==(const edge_view_iterator& rhs) const { return edg_iter_ == rhs; }
+  //constexpr bool operator==(const edgelist_iterator& rhs) const { return edg_iter_ == rhs; }
 
 protected:
-  //friend bool operator==(const edge_iterator& lhs, const edge_view_iterator& rhs) { return lhs == rhs.edg_iter_; }
+  //friend bool operator==(const edge_iterator& lhs, const edgelist_iterator& rhs) { return lhs == rhs.edg_iter_; }
 };
 
 } // namespace std::graph::views
