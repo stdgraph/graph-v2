a0ed8ba7a2cfcf38e065ec2baaf847bc8adcfb30
Phil Ratzloff <pratzl@outlook.com>
2022-04-07 01:21:07 -0400

    Upgrade conan Conan.cmake to use latest technique to specify files
    Catch2.cmake can't be found; it's in the CMAKE_MODULE_PATH, but that
    isn't being used (I must be missing something)
    
    upgrade catch2 2.13.7 to 2.13.8
    upgrade fmt 8.0.1 to 8.1.1
    add boost 1.78

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,10 +1,13 @@
 macro(run_conan)
+  list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
+  list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
+
   # Download automatically, you can also just copy the conan.cmake file
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/f8fd649b3058846188d8b12040c354fd50e30780/conan.cmake"
-                  "${CMAKE_BINARY_DIR}/conan.cmake")
-    # The following should be used, but doesn't work': 'DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.17.0/conan.cmake"
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+                  "${CMAKE_BINARY_DIR}/conan.cmake"
+                  TLS_VERIFY ON)
   endif()
 
   include(${CMAKE_BINARY_DIR}/conan.cmake)
@@ -16,34 +19,24 @@ macro(run_conan)
 
 # See https://github.com/conan-io/cmake-conan for example
 
-# The following conan_cmake_configure and conan_cmake_install are preferred over conan_cmake_run,
-# but they don't appear to be working yet.
 
-#conan_cmake_configure(REQUIRES   catch2/2.13.7
-#                                 docopt.cpp/0.6.3
-#                                 fmt/8.0.1
-#                                 spdlog/1.9.2
-#                                 range-v3/0.11.0
-#                      GENERATORS cmake_find_package)
+  conan_cmake_configure(REQUIRES boost/1.78.0
+                                 catch2/2.13.8
+                                 docopt.cpp/0.6.3
+                                 fmt/8.1.1
+                                 range-v3/0.11.0
+                                 spdlog/1.9.2
+                        GENERATORS cmake_find_package_multi) #cmake_find_package|cmake_find_package_multi?
 
-#conan_cmake_autodetect(settings)
+  conan_cmake_autodetect(settings)
 
-#conan_cmake_install(PATH_OR_REFERENCE .
-#                    BUILD             missing
-#                    REMOTE            conancenter
-#                    SETTINGS          ${settings} compiler.cppstd=20) 
+  conan_cmake_install(PATH_OR_REFERENCE .
+                      BUILD missing
+                      REMOTE conancenter
+                      SETTINGS ${settings})
 
-conan_cmake_run(
-  REQUIRES      ${CONAN_EXTRA_REQUIRES}
-                catch2/2.13.7
-                docopt.cpp/0.6.3
-                fmt/8.0.1
-                spdlog/1.9.2
-                range-v3/0.11.0
-  OPTIONS       ${CONAN_EXTRA_OPTIONS}
-  SETTINGS      compiler.cppstd=20 
-  BASIC_SETUP
-  CMAKE_TARGETS # individual targets to link to
-  BUILD         missing)
+  find_package(boost)
+  find_package(catch2)
+  find_package(fmt)
 
 endmacro()
