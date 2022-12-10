c6ed28bdf579c558403a08574b7193e3068321de
neoblizz <osama94@gmail.com>
2022-12-10 13:58:21 -0800

    Working implementation.

--- include/graph/algorithm/pagerank.hpp
+++ include/graph/algorithm/pagerank.hpp
@@ -34,52 +34,87 @@ template <class G, class F>
 concept edge_weight_function = // e.g. weight(uv)
       copy_constructible<F> && is_arithmetic_v<invoke_result_t<F, edge_reference_t<G>>>;
 
+
+/**
+ * @brief PageRank (PR) algorithm.
+ * 
+ * @tparam G The graph type.
+ * @tparam PageRank The ranks range type.
+ * @tparam EVF The edge value function that returns the weight of an edge.
+ * 
+ * @param g The adjacency graph. 
+ * @param page_rank [out] The page rank of each vertex in the graph, accessible through page_rank[uid], where uid is the vertex_id. The caller must assure size(page_rank) >= size(vertices(g)).
+ * @param damping_factor The alpha/damping factor (default = 0.85.)
+ * @param threshold The error threshold for convergence (default = 1e-4.)
+ * @param max_iterations Maximum number of iterations for convergence (default = std::numeric_limits<unsigned int>::max().) 
+ * @param weight_fn The edge value function (default returns 1 for each edge value.)
+ */
 template <adjacency_list              G,
           ranges::random_access_range PageRank,
-          typename type_t,
           class EVF = std::function<ranges::range_value_t<PageRank>(edge_reference_t<G>)>>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>> &&
          is_arithmetic_v<ranges::range_value_t<PageRank>> && edge_weight_function<G, EVF>
 void pagerank(
-      G&&               g,              // graph
-      PageRank&         page_rank,      // out: page rank scores
-      const type_t      damping_factor, // in: Damping factor (generally set to 0.85)
-      const type_t      threshold,      // in: Error threshold to control converge rate
-      const std::size_t max_iterations, // in: Maximum number of iterations to converge
-      EVF weight = [](edge_reference_t<G> uv) { return ranges::range_value_t<PageRank>(1); }) // default weight(uv) -> 1
-{
+      G&&               g,         // graph
+      PageRank&         page_rank, // out: page rank scores
+      const double      damping_factor = 0.85,
+      const double      threshold      = 1e-4,
+      const std::size_t max_iterations = std::numeric_limits<unsigned int>::max(),
+      EVF               weight_fn      = [](edge_reference_t<G> uv) { return ranges::range_value_t<PageRank>(1); }) {
   using id_type     = vertex_id_t<G>;
   using weight_type = ranges::range_value_t<PageRank>;
 
-  const type_t init_score = 1.0 / size(vertices(g));
-  const type_t base_score = (1.0 - damping_factor) / size(vertices(g));
+  std::vector<weight_type> plast(size(vertices(g)));
+  std::vector<id_type>     degrees(size(vertices(g)));
+  // alpha * 1 / (sum of outgoing weights) -- used to determine
+  // out of mass spread from src to dst
+  std::vector<weight_type> iweights(size(vertices(g)));
 
-  std::vector<id_type> degrees(size(vertices(g)));
-  std::vector<type_t>  outgoing_contrib(size(vertices(g)));
-
-  std::ranges::fill(page_rank, init_score);
+  // Initialize the data, pagerank as 1/n_vertices.
+  std::ranges::fill(page_rank, 1.0 / size(vertices(g)));
 
   for (auto&& [uid, u] : views::vertexlist(g)) {
-    degrees[uid] = degree(g, u);
+    // Calculate the degree of each vertex.
+    size_t edge_cnt = 0;
+    for (auto&& uv : edges(g, u)) {
+      ++edge_cnt;
+    }
+    degrees[uid] = edge_cnt;
+
+    // Find the sum of outgoing weights.
+    weight_type val = 0;
+    for (auto&& [vid, uv, w] : views::incidence(g, uid, weight_fn)) {
+      val += w;
+    }
+    iweights[uid] = (val != 0) ? damping_factor / val : 0;
   }
 
-  for (size_t iter = 0; iter < max_iterations; ++iter) {
+  size_t iter;
+  for (iter = 0; iter < max_iterations; ++iter) {
+    // Make a copy of pagerank from previous iteration.
+    std::ranges::copy(page_rank.begin(), page_rank.end(), plast.begin());
+
+    // Handle "dangling nodes" (nodes w/ zero outdegree)
+    // could skip this if no nodes have sero outdegree
+    weight_type dsum = 0.0f;
+    for (auto&& [uid, u] : views::vertexlist(g)) {
+      dsum += (iweights[uid] == 0) ? damping_factor * page_rank[uid] : 0;
+    }
+    std::ranges::fill(page_rank, (1 - damping_factor + dsum) / size(vertices(g)));
+
     double error = 0;
-    std::transform(page_rank.begin(), page_rank.end(), degrees.begin(), outgoing_contrib.begin(),
-                   [&](auto&& x, auto&& y) { return x / (y + 0); });
-
-
-    for (auto&& [uid, vid, uv] : views::edgelist(g)) {
-      type_t z = 0;
-      z += outgoing_contrib[vid];
-      auto old_rank  = page_rank[uid];
-      page_rank[uid] = base_score + damping_factor * z;
-      error += fabs(page_rank[uid] - old_rank);
+    for (auto&& [uid, vid, uv, val] : views::edgelist(g, weight_fn)) {
+      weight_type update = plast[uid] * iweights[uid] * val;
+      page_rank[vid] += update;
+      error += fabs(page_rank[uid] - plast[uid]);
     }
 
+    // Check for convergence
     if (error < threshold)
       break;
   }
+}
+
 } // namespace std::graph
 
 #endif // GRAPH_PAGERANK_HPP
--- test/pagerank_tests.cpp
+++ test/pagerank_tests.cpp
@@ -1,8 +1,8 @@
 #include <catch2/catch.hpp>
 #include "csv_routes.hpp"
 #include "graph/graph.hpp"
-#include "graph/algorithm/pagerank.hpp"
 #include "graph/container/dynamic_graph.hpp"
+#include "graph/algorithm/pagerank.hpp"
 #include "graph/views/incidence.hpp"
 #include <cassert>
 #ifdef _MSC_VER
@@ -12,11 +12,10 @@
 #define TEST_OPTION_OUTPUT (1) // output tests for visual inspection
 #define TEST_OPTION_GEN (2)    // generate unit test code to be pasted into this file
 #define TEST_OPTION_TEST (3)   // run unit tests
-#define TEST_OPTION TEST_OPTION_TEST
+#define TEST_OPTION TEST_OPTION_OUTPUT
 
 using std::cout;
 using std::endl;
-using std::vector;
 
 using std::graph::vertex_t;
 using std::graph::vertex_id_t;
@@ -34,14 +33,21 @@ using std::graph::edge_value;
 using std::graph::find_vertex;
 using std::graph::vertex_id;
 
-using routes_volf_graph_traits = std::graph::container::vofl_graph_traits<double, std::string>;
+using routes_volf_graph_traits = std::graph::container::vofl_graph_traits<double, std::string, std::string>;
 using routes_volf_graph_type   = std::graph::container::dynamic_adjacency_graph<routes_volf_graph_traits>;
 
 TEST_CASE("PageRank", "[pagerank]") {
   init_console();
-  using G             = routes_volf_graph_type;
-  auto&& g            = load_graph<G>(TEST_DATA_ROOT_DIR "germany_routes.csv");
+  using G  = routes_volf_graph_type;
+  auto&& g = load_ordered_graph<G>(TEST_DATA_ROOT_DIR "germany_routes.csv", name_order_policy::source_order_found);
 
-  vector<double>         page_rank(size(vertices(g)));
-  // pagerank(g, page_rank, float(0.85), float(1e-4), 100);
+  std::vector<double> page_rank(size(vertices(g)));
+  std::graph::pagerank(g, page_rank, double(0.85), double(1e-4), 100);
+
+  std::vector<double> answer = {0.0510883, 0.0655634, 0.106817,  0.141883, 0.0655634,
+                                0.0789528, 0.0655634, 0.0789528, 0.260973, 0.0846433};
+
+  for (size_t idx = 0; idx < page_rank.size(); ++idx) {
+    REQUIRE(page_rank[idx] == Approx(answer[idx]).epsilon(1e-4));
+  }
 }
