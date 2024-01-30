49fedd5cf431576618ab0cff83d58fba9bd46db7
Phil Ratzloff <pratzl@outlook.com>
2024-01-30 08:22:47 -0500

    Reorg concepts for review

--- include/graph/graph.hpp
+++ include/graph/graph.hpp
@@ -68,7 +68,7 @@ namespace std::graph {
  * @ingroup graph_concepts
  * @brief Concept for a targeted edge.
  * 
- * A targeted edge has both @c target_id(g,uv) and @c target(g,uv) functions defined for it.
+ * A basic targeted edge has only the @c target_id(g,uv) function defined for it.
  * 
  * @tparam G The graph type.
  * @tparam E The edge type.
@@ -84,9 +84,9 @@ concept targeted_edge = requires(G&& g, edge_reference_t<G> uv) {
 
 /**
  * @ingroup graph_concepts
- * @brief Concept for a sourced edge.
+ * @brief Concept for a targeted edge.
  * 
- * A sourced edge has both @c source_id(g,uv) and @c source(g,uv) functions defined for it.
+ * A normal targeted edge has both the @c target_id(g,uv) and @c target(g,uv) function defined for it.
  * 
  * @tparam G The graph type.
  * @tparam E The edge type.
@@ -105,7 +105,8 @@ concept basic_sourced_targeted_edge = basic_targeted_edge<G, E> && basic_sourced
                                       requires(G&& g, edge_reference_t<G> uv) { edge_id(g, uv); };
 
 template <class G, class E>
-concept sourced_targeted_edge = targeted_edge<G, E> && sourced_edge<G, E> && //
+concept sourced_targeted_edge = targeted_edge<G, E> && //
+                                sourced_edge<G, E> &&  //
                                 requires(G&& g, edge_reference_t<G> uv) { edge_id(g, uv); };
 
 
@@ -143,25 +144,24 @@ concept key_vertex_range = _common_vertex_range<vertex_range_t<G>> && //
  * @brief Concept for a target edge range
 */
 template <class G>
-concept basic_target_edge_range = requires(G&& g, vertex_id_t<G> uid) {
+concept basic_targeted_edge_range = requires(G&& g, vertex_id_t<G> uid) {
   { edges(g, uid) } -> ranges::forward_range;
 };
 
 template <class G>
-concept target_edge_range = basic_target_edge_range<G> && //
-                            requires(G&& g, vertex_reference_t<G> u) {
-                              { edges(g, u) } -> ranges::forward_range;
-                            };
+concept targeted_edge_range = basic_targeted_edge_range<G> && //
+                              requires(G&& g, vertex_reference_t<G> u) {
+                                { edges(g, u) } -> ranges::forward_range;
+                              };
 
-//template <class G>
-//concept basic_adjacency_list = targeted_edge<G, edge_t<G>> && edge_functions<G>;
-
+//--------------------------------------------------------------------------------------------
 
 /**
  * @ingroup graph_concepts
  * @brief Concept for an adjacency list graph.
  * 
- * An basic_adjacency_list list defines the minimal adjacency list concepts without a vertex object.
+ * A basic_adjacency_list list defines the minimal adjacency list concept with a vertex_id
+ * and without a vertex object.
  * 
  * @tparam G The graph type.
 */
