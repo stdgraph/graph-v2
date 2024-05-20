6ebaf10e9a5b7e4901146f6e360167ccee32481f
Phil Ratzloff <pratzl@outlook.com>
2024-05-19 23:34:35 -0400

    Change edgelist concepts names to use similar conventions as adjlist
    Change namespace ELConcepts --> edgelist

--- include/graph/graph.hpp
+++ include/graph/graph.hpp
@@ -484,7 +484,13 @@ public:
 //
 
 // Target concepts for edgelist
-namespace ELConcepts {
+namespace edgelist {
+  // move implementations for source_id(e), target_id(e), edge_value(e) into std::graph::edgelist?
+  // other functions to support: num_edges(el), contains_edge(uid,vid), edge_id(e)
+
+  //
+  // edgelist ranges
+  //
   template <class E> // For exposition only
   concept _source_target_id = requires(E e) {
     { source_id(e) };
@@ -501,27 +507,47 @@ namespace ELConcepts {
   };
 
   template <class EL> // For exposition only
-  concept basic_edgelist_range = ranges::forward_range<EL> && _source_target_id<ranges::range_value_t<EL>>;
+  concept basic_sourced_edgelist = ranges::forward_range<EL> && _source_target_id<ranges::range_value_t<EL>>;
 
   template <class EL> // For exposition only
-  concept basic_index_edgelist_range = ranges::forward_range<EL> && _index_source_target_id<ranges::range_value_t<EL>>;
+  concept basic_sourced_index_edgelist = ranges::forward_range<EL> && _index_source_target_id<ranges::range_value_t<EL>>;
 
   template <class EL> // For exposition only
-  concept edgelist_range = basic_edgelist_range<EL> && _has_edge_value<ranges::range_value_t<EL>>;
+  concept sourced_edgelist = basic_sourced_edgelist<EL> && _has_edge_value<ranges::range_value_t<EL>>;
 
   template <class EL> // For exposition only
-  concept index_edgelist_range = basic_index_edgelist_range<EL> && _has_edge_value<ranges::range_value_t<EL>>;
+  concept sourced_index_edgelist = basic_sourced_index_edgelist<EL> && _has_edge_value<ranges::range_value_t<EL>>;
 
-  template <basic_edgelist_range EL> // For exposition only
+  //
+  // edgelist types (note that concepts don't really do anything except document expectations)
+  //
+  template <basic_sourced_edgelist EL> // For exposition only
+  using edge_iterator_t = ranges::iterator_t<EL>;
+
+  template <basic_sourced_edgelist EL> // For exposition only
   using edge_t = ranges::range_value_t<EL>;
 
-  template <basic_edgelist_range EL> // For exposition only
+  template <basic_sourced_edgelist EL> // For exposition only
+  using edge_reference_t = ranges::range_reference_t<EL>;
+
+  template <basic_sourced_edgelist EL> // For exposition only
   using vertex_id_t = decltype(source_id(declval<edge_t<EL>>()));
 
-  template <edgelist_range EL> // For exposition only
+  template <sourced_edgelist EL> // For exposition only
   using edge_value_t = decltype(edge_value(declval<edge_t<EL>>()));
 
-} // namespace ELConcepts
+  template <sourced_edgelist EL> // For exposition only
+  using edge_id_t = edge_descriptor<vertex_id_t<EL>,true,void,void>;
+
+  // template aliases can't be distinguished with concepts :(
+  //
+  //template <basic_adjacency_list G> // For exposition only
+  //using vid_t = decltype(vertex_id(declval<G>()));
+  //
+  //template <basic_sourced_edgelist EL> // For exposition only
+  //using vid_t = decltype(source_id(declval<edge_t<EL>>()));
+
+} // namespace edgelist
 
 
 } // namespace std::graph
