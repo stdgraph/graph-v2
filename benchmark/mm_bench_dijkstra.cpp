012983d704d2dab66f75bdffa39ef2a2dbdfacd1
Phil Ratzloff <pratzl@outlook.com>
2024-08-02 11:20:50 -0400

    Add examine_vertex as benchmark option

--- benchmark/mm_bench_dijkstra.cpp
+++ benchmark/mm_bench_dijkstra.cpp
@@ -28,13 +28,15 @@ using fmt::println;
 using namespace std::graph;
 using namespace std::graph::experimental;
 
-#define ENABLE_DISCOVER_VERTEX 1
+//#define ENABLE_DISCOVER_VERTEX 1
+#define ENABLE_EXAMINE_VERTEX 1
 #define ENABLE_EDGE_RELAXED 1
 
 constexpr const size_t test_trials = 3;
 
 struct bench_results {
   size_t vertices_discovered = 0;
+  size_t vertices_examined   = 0;
   size_t edges_relaxed       = 0;
 };
 
@@ -67,6 +69,11 @@ struct discover_vertex_visitor : dijkstra_visitor_base<G> {
 #if ENABLE_DISCOVER_VERTEX
   void on_discover_vertex(const base_t::vertex_desc_type& vdesc) { results_.vertices_discovered += 1; }
 #endif
+#if ENABLE_EXAMINE_VERTEX
+  constexpr void on_examine_vertex(const base_t::vertex_desc_type& vdesc) { results_.vertices_examined += 1; }
+#endif
+#if ENABLE_EXAMINE_VERTEX
+#endif
 #if ENABLE_EDGE_RELAXED
   constexpr void on_edge_relaxed(const base_t::sourced_edge_desc_type& edesc) noexcept { results_.edges_relaxed += 1; }
 #endif
@@ -75,206 +82,6 @@ private:
   bench_results& results_;
 };
 
-#if 0
-template <class G, class DistancesFnc>
-auto bench_visitor_dijkstra(G&                           g,
-                            const array_matrix<int64_t>& sources,
-                            DistancesFnc&                distance_fnc,
-                            bench_results&               results) {
-  std::string desc = fmt::format("Running {}", "visitor_dijkstra");
-
-  double min_elapsed         = std::numeric_limits<double>::max();
-  size_t vertices_discovered = 0;
-  size_t edges_relaxed       = 0;
-
-  using Distance     = int64_t;
-  using Distances    = std::vector<Distance>;
-  using Predecessors = std::vector<vertex_id_t<G>>;
-
-  Distances    distances(size(vertices(g)));
-  Predecessors predecessors(size(vertices(g)));
-  size_t       reported_vertices_discovered = 0;
-
-  for (size_t t = 0; t < test_trials; ++t) {
-    init_shortest_paths(distances, predecessors);
-    discover_vertex_visitor visitor(g, results);
-    {
-      dijkstra_with_visitor(g, visitor, sources.vals, predecessors, distances, distance_fnc);
-
-      reported_vertices_discovered = visitor.vertices_discovered();
-      //run_time.set_count(visitor.vertices_discovered(), "vertice(s) discovered"); // includes sources
-    }
-  }
-
-  output_vertices_visited(distances);
-#  if ENABLE_DISCOVER_VERTEX
-  fmt::println("{:L} vertice(s) were discovered", visitor.vertices_discovered()); // includes sources
-#  endif
-#  if ENABLE_EDGE_RELAXED
-  fmt::println("{:L} edge(s) were relaxed", visitor.edges_relaxed());
-#  endif
-};
-
-
-//-------------------------------------------------------------------------------------------------
-// bench_co_dijkstra
-//
-template <class G, class DistancesFnc>
-auto bench_co_dijkstra(G& g, const array_matrix<int64_t>& sources, DistancesFnc& distance_fnc) {
-  std::string desc = fmt::format("Running {}", "co_dijkstra");
-
-  size_t          vertices_discovered = 0;
-  size_t          edges_relaxed       = 0;
-  dijkstra_events events              = dijkstra_events::none;
-#  if ENABLE_DISCOVER_VERTEX
-  events |= dijkstra_events::discover_vertex;
-#  endif
-#  if ENABLE_EDGE_RELAXED
-  events |= dijkstra_events::edge_relaxed;
-#  endif
-
-  using Distance     = int64_t;
-  using Distances    = std::vector<Distance>;
-  using Predecessors = std::vector<vertex_id_t<G>>;
-
-  Distances    distances(size(vertices(g)));
-  Predecessors predecessors(size(vertices(g)));
-
-  init_shortest_paths(distances, predecessors);
-
-  {
-    timer run_time(desc, true);
-    for (auto bfs = co_dijkstra(g, events, sources.vals, predecessors, distances, distance_fnc); bfs;) {
-      auto&& [event, payload] = bfs();
-      switch (event) {
-#  if ENABLE_DISCOVER_VERTEX
-      case dijkstra_events::discover_vertex: {
-        auto&& [uid, u] = get<bfs_vertex_value_t<G>>(payload); // or get<1>(payload);
-        ++vertices_discovered;
-      } break;
-#  endif
-#  if ENABLE_EDGE_RELAXED
-      case dijkstra_events::edge_relaxed: {
-        auto&& [uid, vid, uv] = get<bfs_edge_value_t<G>>(payload); // or get<1>(payload);
-        ++edges_relaxed;
-      } break;
-#  endif
-      default: break;
-      }
-    }
-  }
-
-  output_vertices_visited(distances);
-#  if ENABLE_DISCOVER_VERTEX
-  fmt::println("{:L} vertice(s) were discovered", vertices_discovered); // includes sources
-#  endif
-#  if ENABLE_EDGE_RELAXED
-  fmt::println("{:L} edge(s) were relaxed", edges_relaxed);
-#  endif
-};
-
-//-------------------------------------------------------------------------------------------------
-// bench_nwgraph_dijkstra
-//
-template <class G, class DistancesFnc>
-auto bench_nwgraph_dijkstra(G& g, const array_matrix<int64_t>& sources, DistancesFnc& distance_fnc) {
-  std::string desc = fmt::format("Running {}", "nwgraph_dijkstra");
-  using Distance   = int64_t;
-  using Distances  = std::vector<Distance>;
-  //using Predecessors = std::vector<vertex_id_t<G>>;
-
-  //auto distance_fnc = [&g](std::tuple<int64_t, int64_t>& uv) { return std::get<1>(uv); };
-
-
-  Distances distances;
-  //Predecessors predecessors(size(vertices(g)));
-  //size_t       reported_vertices_discovered = 0;
-  {
-    timer run_time(desc, true);
-
-    //init_shortest_paths(distances, predecessors);
-
-    //discover_vertex_visitor visitor(g, distances);
-    //dijkstra_with_visitor(g, visitor, sources.vals, predecessors, distances, distance_fnc);
-    distances = nwgraph_dijkstra(g, static_cast<vertex_id_t<G>>(sources.vals[0]), distance_fnc);
-
-    //reported_vertices_discovered = visitor.vertices_discovered();
-    //run_time.set_count(visitor.vertices_discovered(), "vertice(s) discovered"); // includes sources
-  }
-
-  // A vertex may actually visited more than once for ragged graphs, where the same vertex is visited from different
-  // sources that have a shorter path to the vertex.
-  {
-    size_t actual_vertices_discovered = 0;
-    for (auto d : distances) {
-      if (d != shortest_path_invalid_distance<Distance>())
-        ++actual_vertices_discovered;
-    }
-
-    fmt::println("Vertices discovered was ({:L})", actual_vertices_discovered);
-    fmt::println("{:1.1f}% of all vertices were visited", //
-                 100.0 * static_cast<double>(actual_vertices_discovered) / static_cast<double>(num_vertices(g)));
-  }
-};
-
-
-//-------------------------------------------------------------------------------------------------
-// bench_dijkstra_main
-//
-void bench_dijkstra_main() {
-  using G = vector<vector<tuple<int64_t, int64_t>>>;
-
-  fmt::println("Benchmarking Dijkstra's Algorithm Using Visitors and Co-routines");
-  fmt::println("================================================================");
-  fmt::println("Benchmark starting at {}", current_timestamp());
-  cout << endl;                                             // endl forces buffer flush; needed for Linux
-
-  bench_files                      bench_source = gap_road; // gap_road, g2bench_bips98_606, g2bench_chesapeake
-  triplet_matrix<int64_t, int64_t> triplet;
-  array_matrix<int64_t>            sources;
-
-  bool const requires_sort = !std_adjacency_graph<G>;
-  load_matrix_market(bench_source, triplet, sources, requires_sort); // gap_road, g2bench_bips98_606, g2bench_chesapeake
-  cout << endl;
-
-  G           g;
-  graph_stats stats = load_graph(triplet, g);
-  fmt::println("Graph stats: {}", stats);
-  cout << endl;
-
-  // Use first source only (temporary)
-  sources.ncols = 1;
-  sources.nrows = 1;
-  sources.vals.erase(sources.vals.begin() + 1, sources.vals.end());
-
-  fmt::println("{:L} source(s) will be used", sources.nrows);
-
-#  if 1
-  auto distance_fnc = [&g](edge_reference_t<G> uv) -> int64_t { return std::get<1>(edge_value(g, uv)); };
-  println("Edge weight function = edge_value(g, uv)");
-#  else
-  auto distance_fnc = [&g](edge_reference_t<G> uv) -> int64_t { return 1; };
-  println("Edge weight function = 1");
-#  endif
-  cout << endl;
-
-
-  // Run the algorithm
-  try {
-    {
-      //bench_nwgraph_dijkstra(g, sources, distance_fnc);
-      //cout << endl;
-      bench_visitor_dijkstra(g, sources, distance_fnc);
-      cout << endl;
-      bench_co_dijkstra(g, sources, distance_fnc);
-    }
-  } catch (const std::exception& e) {
-    fmt::print("Exception caught: {}\n", e.what());
-  }
-
-  cout << endl;
-}
-#endif // 0
 
 //-------------------------------------------------------------------------------------------------
 // bench_dijkstra_runner
@@ -319,6 +126,9 @@ void bench_dijkstra_runner() {
 #if ENABLE_DISCOVER_VERTEX
   events += "discover_vertex ";
 #endif
+#if ENABLE_EXAMINE_VERTEX
+  events += "examine_vertex ";
+#endif
 #if ENABLE_EDGE_RELAXED
   events += "edge_relaxed ";
 #endif
@@ -345,6 +155,9 @@ void bench_dijkstra_runner() {
 #if ENABLE_DISCOVER_VERTEX
           events |= dijkstra_events::discover_vertex;
 #endif
+#if ENABLE_EXAMINE_VERTEX
+          events |= dijkstra_events::examine_vertex;
+#endif
 #if ENABLE_EDGE_RELAXED
           events |= dijkstra_events::edge_relaxed;
 #endif
@@ -355,6 +168,12 @@ void bench_dijkstra_runner() {
             case dijkstra_events::discover_vertex: {
               auto&& [uid, u] = get<bfs_vertex_value_t<G>>(payload); // or get<1>(payload);
               results.vertices_discovered += 1;
+            } break;
+#endif
+#if ENABLE_EXAMINE_VERTEX
+            case dijkstra_events::examine_vertex: {
+              auto&& [uid, u] = get<bfs_vertex_value_t<G>>(payload); // or get<1>(payload);
+              results.vertices_examined += 1;
             } break;
 #endif
 #if ENABLE_EDGE_RELAXED
@@ -391,6 +210,9 @@ void bench_dijkstra_runner() {
 #if ENABLE_DISCOVER_VERTEX
     fmt::print("  Vertices Discovered");
 #endif
+#if ENABLE_EXAMINE_VERTEX
+    fmt::print("  Vertices Examined");
+#endif
 #if ENABLE_EDGE_RELAXED
     fmt::print("  Edges Relaxed");
 #endif
@@ -414,6 +236,9 @@ void bench_dijkstra_runner() {
         print("{:<{}}  {:>11L}  {:>11.3f}", algo.name, name_max, one_source[0], min_elapsed);
 #if ENABLE_DISCOVER_VERTEX
         print("  {:>19L}", results.vertices_discovered);
+#endif
+#if ENABLE_EXAMINE_VERTEX
+        print("  {:>17L}", results.vertices_examined);
 #endif
 #if ENABLE_EDGE_RELAXED
         print("  {:>13L}", results.edges_relaxed);
