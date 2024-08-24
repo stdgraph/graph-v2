189e8b8ae10a26073d4755b0f61b5f96b6b32516
kdeweese <kgd.baf@gmail.com>
2024-08-24 12:28:10 -0700

    added kosaraju for directed SCC and an undirected connected components based on DFS

---
+++ include/graph/algorithm/connected_components.hpp
@@ -0,0 +1,115 @@
+/**
+ * @file connected_components.hpp
+ * 
+ * @brief Single-Source Shortest paths and shortest sistances algorithms using Dijkstra & 
+ * Bellman-Ford algorithms.
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * SPDX-License-Identifier: BSL-1.0
+ *
+ * @authors
+ *   Andrew Lumsdaine
+ *   Phil Ratzloff
+ *   Kevin Deweese
+ */
+
+#include "graph/graph.hpp"
+#include "graph/views/incidence.hpp"
+
+#ifndef GRAPH_CC_HPP
+#  define GRAPH_CC_HPP
+
+namespace std::graph {
+
+template <adjacency_list G,
+          adjacency_list GT,
+          ranges::random_access_range Component>
+requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
+void kosaraju(G&&            g,        // graph
+              GT&&           g_t,      // graph transpose
+              Component&     component // out: strongly connected component assignment
+
+) {
+  size_t N(size(vertices(g)));
+  std::vector<bool> visited(N, false);
+  using CT = typename std::decay<decltype(*component.begin())>::type;
+  std::fill(component.begin(), component.end(), std::numeric_limits<CT>::max());
+  std::vector<vertex_id_t<G>> order;
+  
+  for (auto&& [uid, u] : views::vertexlist(g)) {
+    if (visited[uid]) {
+        continue;
+    }
+    visited[uid] = true;
+    std::stack<vertex_id_t<G>> active;
+    active.push(uid);
+    auto dfs = std::graph::views::sourced_edges_depth_first_search(g, uid);
+    for (auto&& [vid, wid, vw] : dfs) {
+        while (vid != active.top()) {
+          order.push_back(active.top());
+          active.pop();
+        }
+        if (visited[wid]) {
+          dfs.cancel(cancel_search::cancel_branch);
+        }
+        else {
+          active.push(wid);
+          visited[wid] = true;
+        }
+    }
+    while (!active.empty()) {
+      order.push_back(active.top());
+      active.pop();
+    }
+  }
+
+  size_t cid = 0;
+  std::ranges::reverse_view reverse {order};
+  for (auto& uid : reverse) {
+    if (component[uid] == std::numeric_limits<CT>::max()) {
+      component[uid] = cid;
+      vertices_depth_first_search_view<GT> dfs(g_t, uid);
+      for (auto&& [vid, v] : dfs) {
+        if (component[vid] != std::numeric_limits<CT>::max()) {
+          dfs.cancel(cancel_search::cancel_branch);
+        }
+        else {
+          component[vid] = cid;
+        }
+      }
+      ++cid;
+    }
+  }
+}
+
+template <adjacency_list G,
+          ranges::random_access_range Component>
+requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
+void connected_components(G&&            g,        // graph
+                          Component&     component // out: connected component assignment
+) {
+  size_t N(size(vertices(g)));
+  std::vector<bool> visited(N, false);
+  using CT = typename std::decay<decltype(*component.begin())>::type;
+  std::fill(component.begin(), component.end(), std::numeric_limits<CT>::max());
+  
+  CT cid = 0;
+  for (auto&& [uid, u] : views::vertexlist(g)) {
+    if (visited[uid]) {
+      continue;
+    }
+    visited[uid] = true;
+    component[uid] = cid;
+    vertices_depth_first_search_view<G, void> dfs(g, uid);
+    for (auto&& [vid, v] : dfs) {
+      component[vid] = cid;
+      visited[vid] = true;
+    }
+    ++cid;
+  }
+}
+
+} // namespace std::graph
+
+#endif //GRAPH_CC_HPP
