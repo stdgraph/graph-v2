d66a18398d1bea89e32d8b5c3854179397298608
Phil Ratzloff <pratzl@outlook.com>
2024-09-18 18:22:57 -0400

    Add "For exposition only" to visitor concepts

--- include/graph/algorithm/common_shortest_paths.hpp
+++ include/graph/algorithm/common_shortest_paths.hpp
@@ -81,49 +81,49 @@ constexpr void init_shortest_paths(Distances& distances, Predecessors& predecess
 
 // Vertex visitor concepts
 template <class G, class Visitor>
-concept has_on_initialize_vertex = //
+concept has_on_initialize_vertex = // For exposition only
       requires(Visitor& v, vertex_descriptor<vertex_id_t<G>, vertex_reference_t<G>, void> vdesc) {
         { v.on_initialize_vertex(vdesc) };
       };
 template <class G, class Visitor>
-concept has_on_discover_vertex = //
+concept has_on_discover_vertex = // For exposition only
       requires(Visitor& v, vertex_descriptor<vertex_id_t<G>, vertex_reference_t<G>, void> vdesc) {
         { v.on_discover_vertex(vdesc) };
       };
 template <class G, class Visitor>
-concept has_on_examine_vertex = //
+concept has_on_examine_vertex = // For exposition only
       requires(Visitor& v, vertex_descriptor<vertex_id_t<G>, vertex_reference_t<G>, void> vdesc) {
         { v.on_examine_vertex(vdesc) };
       };
 template <class G, class Visitor>
-concept has_on_finish_vertex = //
+concept has_on_finish_vertex = // For exposition only
       requires(Visitor& v, vertex_descriptor<vertex_id_t<G>, vertex_reference_t<G>, void> vdesc) {
         { v.on_finish_vertex(vdesc) };
       };
 
 // Edge visitor concepts
 template <class G, class Visitor>
-concept has_on_examine_edge = //
+concept has_on_examine_edge = // For exposition only
       requires(Visitor& v, edge_descriptor<vertex_id_t<G>, true, edge_reference_t<G>, void> edesc) {
         { v.on_examine_edge(edesc) };
       };
 template <class G, class Visitor>
-concept has_on_edge_relaxed = //
+concept has_on_edge_relaxed = // For exposition only
       requires(Visitor& v, edge_descriptor<vertex_id_t<G>, true, edge_reference_t<G>, void> edesc) {
         { v.on_edge_relaxed(edesc) };
       };
 template <class G, class Visitor>
-concept has_on_edge_not_relaxed = //
+concept has_on_edge_not_relaxed = // For exposition only
       requires(Visitor& v, edge_descriptor<vertex_id_t<G>, true, edge_reference_t<G>, void> edesc) {
         { v.on_edge_not_relaxed(edesc) };
       };
 template <class G, class Visitor>
-concept has_on_edge_minimized = //
+concept has_on_edge_minimized = // For exposition only
       requires(Visitor& v, edge_descriptor<vertex_id_t<G>, true, edge_reference_t<G>, void> edesc) {
         { v.on_edge_minimized(edesc) };
       };
 template <class G, class Visitor>
-concept has_on_edge_not_minimized =
+concept has_on_edge_not_minimized = // For exposition only
       requires(Visitor& v, edge_descriptor<vertex_id_t<G>, true, edge_reference_t<G>, void> edesc) {
         { v.on_edge_not_minimized(edesc) };
       };
