629397999591ceeca532e0a127cfebb48c96daac
Andrzej Krzemienski <akrzemi1@gmail.com>
2025-06-11 02:21:24 +0200

    Fix MSVC build

--- tests/compressed_graph_tests.cpp
+++ tests/compressed_graph_tests.cpp
@@ -1,3 +1,9 @@
+// Copyright (C) 2025 Andrzej Krzemienski.
+//
+// Use, modification, and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_template_test_macros.hpp>
 #include "graph/container/compressed_graph.hpp"
